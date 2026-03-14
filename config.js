671451fec9cda0a0fc2fa65b8ab4aff963b29a85
tomo/えありも <earimokon1170@gmail.com>
2026-03-14 13:57:55 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: challenge, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'tiktok.com'.
     interstellar: "tiktok.com",
